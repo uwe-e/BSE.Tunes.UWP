dc469dbca205eada105c2611b0569c264f67898b
uwe-e <git.bsetunes@outlook.com>
2022-02-07 18:19:10 +0100

    AppTitleBar modified

--- src/Services/ThemeSelectorService.cs
+++ src/Services/ThemeSelectorService.cs
@@ -6,7 +6,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.Core;
 using Windows.Storage;
+using Windows.UI;
 using Windows.UI.Core;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 
 namespace BSE.Tunes.StoreApp.Services
@@ -19,19 +21,22 @@ namespace BSE.Tunes.StoreApp.Services
 
         public static async Task InitializeAsync()
         {
+            UpdateTitleBar();
             Theme = await LoadThemeFromSettingsAsync();
         }
 
         public static async Task SetThemeAsync(ElementTheme theme)
         {
             Theme = theme;
-
+            
             await SetRequestedThemeAsync();
             await SaveThemeInSettingsAsync(Theme);
         }
 
         public static async Task SetRequestedThemeAsync()
         {
+            UpdateSystemCaptionButtonColors();
+
             foreach (var view in CoreApplication.Views)
             {
                 await view.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
@@ -60,6 +65,37 @@ namespace BSE.Tunes.StoreApp.Services
         private static async Task SaveThemeInSettingsAsync(ElementTheme theme)
         {
             await ApplicationData.Current.LocalSettings.SaveAsync(SettingsKey, theme.ToString());
+        }
+
+        private static bool IsDarkTheme()
+        {
+            if (Theme == ElementTheme.Default)
+            {
+                return Application.Current.RequestedTheme == ApplicationTheme.Dark;
+            }
+            return Theme == ElementTheme.Dark;
+        }
+
+        private static void UpdateSystemCaptionButtonColors()
+        {
+            ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+
+            if (IsDarkTheme())
+            {
+                titleBar.ButtonForegroundColor = Colors.White;
+            }
+            else
+            {
+                titleBar.ButtonForegroundColor = Colors.Black;
+            }
+        }
+
+        private static void UpdateTitleBar()
+        {
+            ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+
+            titleBar.ButtonBackgroundColor = Colors.Transparent;
+            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
         }
     }
 }
